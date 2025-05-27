da929fad891a92202647ce857b968b42e8b9288a
Vasudevan <vasudevan.r@krossark.com>
2025-05-27 12:19:09 +0530

    updated home page changes

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,5 +1,5 @@
-//export const BASE_API_URL = "http://localhost:8000";
-export const BASE_API_URL = "https://nutribk.onrender.com";
+export const BASE_API_URL = "http://localhost:8000";
+// export const BASE_API_URL = "https://nutribk.onrender.com";
 
 // NAVIGATION
 export const NAV_LINKS = [
