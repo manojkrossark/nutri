ce4032ada90666554da96bb6bf8a2f6edbc42254
Vasudevan <vasudevan.r@krossark.com>
2025-06-17 13:26:44 +0530

    updated nutri  login and footer

--- src/layouts/headers/HeaderThree.tsx
+++ src/layouts/headers/HeaderThree.tsx
@@ -78,7 +78,7 @@ const HeaderThree = () => {
                 </Link>
               </div>
 
-              <div className="right-widget ms-auto me-3 me-lg-0 order-lg-4">
+              {/* <div className="right-widget ms-auto me-3 me-lg-0 order-lg-4">
                 <ul className="d-flex align-items-center style-none">
                   <li
                     className="d-flex align-items-center login-btn-one"
@@ -101,42 +101,8 @@ const HeaderThree = () => {
                       </span>
                     </Link>
                   </li>
-                  {/* <li className="d-none d-md-inline-block ms-3 ms-xl-4 me-xl-4  text-white"> */}
-                  {/*  <Link href="/dashboard/add-property" className="btn-five md rounded-0" target="_blank"><span>Add Listing</span> <i className="fa-thin fa-arrow-up-right"></i></Link> */}
-                  {/* </li> */}
-                  {/* <li className="d-none d-xl-block  text-white">
-                    <button
-                      onClick={() => setOffCanvas(true)}
-                      style={{ cursor: "pointer" }}
-                      className="sidenavbtn rounded-circle tran3s  text-white"
-                      type="button"
-                    >
-                      <i className="fa-sharp fa-light fa-bars-filter "></i>
-                    </button>
-                  </li> */}
                 </ul>
-              </div>
-
-              {/*    <div className="order-lg-2 d-none d-xxl-block">
-                        <p className="m0 email-text ps-5 pe-5">Our Email <Link href="#" className="tran3s fw-500 ms-2">askhomy@demo.com</Link></p>
-                     </div> */}
-
-              {/* <nav className="navbar navbar-expand-lg p0 ms-lg-5 order-lg-3">
-                <button
-                  className="navbar-toggler d-block d-lg-none"
-                  type="button"
-                  data-bs-toggle="collapse"
-                  data-bs-target="#navbarNav"
-                  aria-controls="navbarNav"
-                  aria-expanded="false"
-                  aria-label="Toggle navigation"
-                >
-                  <span></span>
-                </button>
-                <div className="collapse navbar-collapse" id="navbarNav">
-                  <NavMenu />
-                </div>
-              </nav> */}
+              </div> */}
             </div>
           </div>
         </div>
